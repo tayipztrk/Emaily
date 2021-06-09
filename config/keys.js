393966683b664bac8d1b81a70e74536a64976f9b
Tayipztrk <tayipztrk@icloud.com>
2021-06-09 19:02:27 +0300

    v3

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '834836444632-7bt5meuetd2sfsg4a3tqf4lm9p4ki079.apps.googleusercontent.com',
-    googleClientSecret: 'A66oYluXHRLavKSyUYp_RC0z',
-    mongoURI: 'mongodb+srv://tayipztrk:13579.Kot@cluster0.pwwp7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    cookieKey: 'jlkjfdklajfdajiejijijfdf',
-};
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
+}

b53879686a28ca093d023e24c93ee7c9248ab25c
Tayipztrk <tayipztrk@icloud.com>
2021-06-09 18:59:44 +0300

    auth flow

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '834836444632-7bt5meuetd2sfsg4a3tqf4lm9p4ki079.apps.googleusercontent.com',
+    googleClientSecret: 'A66oYluXHRLavKSyUYp_RC0z',
+    mongoURI: 'mongodb+srv://tayipztrk:13579.Kot@cluster0.pwwp7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'jlkjfdklajfdajiejijijfdf',
+};
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.mongoURI]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0');
+
+app.listen(PORT);
+
+
+//mongodb+srv://tayipztrk:<password>@cluster0.pwwp7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority

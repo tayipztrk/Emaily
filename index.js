1e9e53a9f392b5d20bf66f15ed264f421da4217e
Tayipztrk <tayipztrk@icloud.com>
2021-06-11 00:06:45 +0300

    v4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -17,7 +18,7 @@ const app = express();
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.mongoURI]
+        keys: [keys.cookieKey]
     })
 );
 
@@ -25,10 +26,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === 'production'){
+     // Express will serve up production assets
+     // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 
 
-//mongodb+srv://tayipztrk:<password>@cluster0.pwwp7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
